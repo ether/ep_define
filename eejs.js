9607f77aa4a61f3e0e71c8534177f1317e698dc8
SamTV12345 <40429738+SamTV12345@users.noreply.github.com>
2026-05-26 20:05:57 +0200

    fix: backward-compat for ESM etherpad (#87)
    - Drop trailing slash on ep_etherpad-lite/node/eejs/ require
    
    Backward-compatible with current CJS etherpad release; also
    compatible with the upcoming ESM etherpad branch which has stricter
    exports map resolution.

--- eejs.js
+++ eejs.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const eejs = require('ep_etherpad-lite/node/eejs/');
+const eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_editbarMenuRight = (hookName, args, cb) => {
   args.content = eejs.require('ep_define/templates/define.ejs', {settings: false}) + args.content;
